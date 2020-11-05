76dd7a040fb54784f82caa4cf5f1a79ff15862f0
Dipto <tawsifshahriar7@gmail.com>
2020-11-05 16:33:21 +0600

    TV_SHOW and CELEB view created

--- imdb/celeb/views.py
+++ imdb/celeb/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
+from django.db import connection
 
-# Create your views here.
-from django.shortcuts import render
 
-def process (request):
-    return render(request, 'celeb.html')
+def process(request):
+    with connection.cursor() as cursor:
+        sql = "SELECT * FROM CELEB"
+        cursor.execute(sql)
+        celebs = cursor.fetchall()
+    return render(request, 'celeb.html', {"celebs": celebs})
+
+
+def detail(request, celeb_id):
+    with connection.cursor() as cursor:
+        sql = "SELECT * FROM CELEB WHERE CELEB_ID=%d" % celeb_id
+        cursor.execute(sql)
+        celeb = cursor.fetchall()
+    return render(request, 'detail.html', {"celeb": celeb})
--- imdb/tv_show/views.py
+++ imdb/tv_show/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
+from django.db import connection
 
-def process (request):
-    return render(request, 'tv_show.html')
 
+def process(request):
+    with connection.cursor() as cursor:
+        sql = "SELECT * FROM TV_SHOW"
+        cursor.execute(sql)
+        tv_shows = cursor.fetchall()
+    return render(request, 'tv_show.html', {"tv_shows": tv_shows})
+
+
+def detail(request, show_id):
+    with connection.cursor() as cursor:
+        sql = "SELECT * FROM TV_SHOW WHERE SHOW_ID=%d" % show_id
+        cursor.execute(sql)
+        tv_show = cursor.fetchall()
+    return render(request, 'detail.html', {"tv_show": tv_show})
