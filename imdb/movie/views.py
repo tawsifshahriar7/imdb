f7385fabea90162d54f292000a5854ce1f4749b0
Dipto <tawsifshahriar7@gmail.com>
2020-11-05 14:04:27 +0600

    Movie detailed view created and linkup created

--- imdb/movie/views.py
+++ imdb/movie/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
+from django.db import connection
 
-def process (request):
-    return render(request, 'movie.html')
+
+def process(request):
+    with connection.cursor() as cursor:
+        sql="select * from movie"
+        cursor.execute(sql)
+        movies = cursor.fetchall()
+    return render(request, 'movie.html', {"movies": movies})
+
+
+def detail(request, movie_id):
+    with connection.cursor() as cursor:
+        sql="SELECT * FROM MOVIE WHERE MOVIE_ID=%d" % movie_id
+        cursor.execute(sql)
+        movie_detail=cursor.fetchall()
+    return render(request, 'details.html', {"movie_detail": movie_detail})
 
